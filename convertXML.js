f91ec8e348a892b35b0b59b8a639ca9996b64787
amarcic <aleksander.marcic@gmail.com>
2022-08-29 19:17:35 +0200

    Update convertXML.js

--- convertXML.js
+++ convertXML.js
@@ -3,5 +3,5 @@ var convert = require('xml-js');
 var xml = fs.readFileSync('./DerSturmTest.xml', 'utf8');
 var result = convert.xml2json(xml, {compact: true, spaces: 4});
 
-fs.writeFile('jsonData.json', result);
+fs.writeFileSync('jsonData.json', result);
 console.log(result);
